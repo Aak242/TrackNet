f3977b26f725eea54552da00cff91402e7ad5a71
Aakash S/O Rengarajan Ramaswamy <e1059778@u.nus.edu>
2025-02-09 15:10:41 +0100

    working model training

--- datasets.py
+++ datasets.py
@@ -4,54 +4,97 @@ import pandas as pd
 import cv2
 import math
 import numpy as np
+import torch
 
 class trackNetDataset(Dataset):
     def __init__(self, mode, input_height=360, input_width=640):
-        self.path_dataset = './datasets/trackNet'
+        self.path_dataset = os.path.abspath('./datasets/trackNet')  # Get absolute path
         assert mode in ['train', 'val'], 'incorrect mode'
-        self.data = pd.read_csv(os.path.join(self.path_dataset, 'labels_{}.csv'.format(mode)))
+        self.data = pd.read_csv(os.path.join(self.path_dataset, f'labels_{mode}.csv'))
         print('mode = {}, samples = {}'.format(mode, self.data.shape[0]))         
         self.height = input_height
         self.width = input_width
+        
+        # Clean up the paths - remove any NaN values and ensure string type
+        path_columns = ['path1', 'path2', 'path3', 'gt_path']
+        for col in path_columns:
+            self.data[col] = self.data[col].fillna('').astype(str)
+            # Convert forward slashes to OS-specific separator
+            self.data[col] = self.data[col].apply(lambda x: os.path.normpath(x) if x else '')
+        
+        # Convert coordinate columns to float, filling NaN with -1
+        self.data['X'] = self.data['X'].fillna(-1).astype(float)
+        self.data['Y'] = self.data['Y'].fillna(-1).astype(float)
         
     def __len__(self):
         return self.data.shape[0]
     
     def __getitem__(self, idx):
-        path, path_prev, path_preprev, path_gt, x, y, status, vis = self.data.loc[idx, :]
+        # Get paths
+        path1 = self.data.iloc[idx]['path1']
+        path2 = self.data.iloc[idx]['path2']
+        path3 = self.data.iloc[idx]['path3']
+        gt_path = self.data.iloc[idx]['gt_path']
         
-        path = os.path.join(self.path_dataset, path)
-        path_prev = os.path.join(self.path_dataset, path_prev)
-        path_preprev = os.path.join(self.path_dataset, path_preprev)
-        path_gt = os.path.join(self.path_dataset, path_gt)
-        if math.isnan(x):
-            x = -1
-            y = -1
+        # Skip if any path is empty
+        if not all([path1, path2, path3, gt_path]):
+            # Return zero tensors if paths are missing
+            inputs = torch.zeros((9, self.height, self.width), dtype=torch.float32)
+            outputs = torch.zeros(self.width * self.height, dtype=torch.float32)
+            return inputs, outputs, -1, -1, 0
         
-        inputs = self.get_input(path, path_prev, path_preprev)
-        outputs = self.get_output(path_gt)
+        # Get coordinates and visibility
+        x = float(self.data.iloc[idx]['X'])
+        y = float(self.data.iloc[idx]['Y'])
+        vis = int(self.data.iloc[idx]['Visibility Class'])
         
-        return inputs, outputs, x, y, vis
+        # Construct full paths using os.path.join
+        path1 = os.path.join(self.path_dataset, path1)
+        path2 = os.path.join(self.path_dataset, path2)
+        path3 = os.path.join(self.path_dataset, path3)
+        gt_path = os.path.join(self.path_dataset, gt_path)
+        
+        try:
+            # Read and process images
+            inputs = self.get_input(path1, path2, path3)
+            outputs = self.get_output(gt_path)
+            
+            # Convert to torch tensors with correct types
+            inputs = torch.from_numpy(inputs).float()
+            outputs = torch.from_numpy(outputs).float()
+            
+            return inputs, outputs, x, y, vis
+        except Exception as e:
+            print(f"Error loading images for index {idx}: {str(e)}")
+            # Return zero tensors on error
+            inputs = torch.zeros((9, self.height, self.width), dtype=torch.float32)
+            outputs = torch.zeros(self.width * self.height, dtype=torch.float32)
+            return inputs, outputs, -1, -1, 0
     
     def get_output(self, path_gt):
         img = cv2.imread(path_gt)
+        if img is None:
+            # Return black image if gt image doesn't exist
+            img = np.zeros((self.height, self.width, 3), dtype=np.float32)
         img = cv2.resize(img, (self.width, self.height))
-        img = img[:, :, 0]
+        img = img[:, :, 0]  # Take first channel only
         img = np.reshape(img, (self.width * self.height))
-        return img
+        return img.astype(np.float32)  # Ensure float32 type
         
-    def get_input(self, path, path_prev, path_preprev):
-        img = cv2.imread(path)
-        img = cv2.resize(img, (self.width, self.height))
-
-        img_prev = cv2.imread(path_prev)
-        img_prev = cv2.resize(img_prev, (self.width, self.height))
+    def get_input(self, path1, path2, path3):
+        img1 = cv2.imread(path1)
+        img2 = cv2.imread(path2)
+        img3 = cv2.imread(path3)
         
-        img_preprev = cv2.imread(path_preprev)
-        img_preprev = cv2.resize(img_preprev, (self.width, self.height))
+        if img1 is None or img2 is None or img3 is None:
+            raise ValueError(f"Failed to load images: {path1}, {path2}, {path3}")
+            
+        img1 = cv2.resize(img1, (self.width, self.height))
+        img2 = cv2.resize(img2, (self.width, self.height))
+        img3 = cv2.resize(img3, (self.width, self.height))
         
-        imgs = np.concatenate((img, img_prev, img_preprev), axis=2)
+        imgs = np.concatenate((img1, img2, img3), axis=2)
         imgs = imgs.astype(np.float32)/255.0
-
         imgs = np.rollaxis(imgs, 2, 0)
+        
         return imgs
--- gt_gen.py
+++ gt_gen.py
@@ -11,69 +11,88 @@ def gaussian_kernel(size, variance):
 
 def create_gaussian(size, variance):
     gaussian_kernel_array = gaussian_kernel(size, variance)
-    gaussian_kernel_array =  gaussian_kernel_array * 255/gaussian_kernel_array[int(len(gaussian_kernel_array)/2)][int(len(gaussian_kernel_array)/2)]
+    gaussian_kernel_array = gaussian_kernel_array * 255/gaussian_kernel_array[int(len(gaussian_kernel_array)/2)][int(len(gaussian_kernel_array)/2)]
     gaussian_kernel_array = gaussian_kernel_array.astype(int)
     return gaussian_kernel_array
 
 def create_gt_images(path_input, path_output, size, variance, width, height):
     gaussian_kernel_array = create_gaussian(size, variance)
-    for game_id in range(1,11):
-        game = 'game{}'.format(game_id)
-        clips = os.listdir(os.path.join(path_input, game))
-        for clip in clips:
-            print('game = {}, clip = {}'.format(game, clip))
+    game = 'game1'  # Assuming we're processing game1 directory
+    clips = os.listdir(os.path.join(path_input, game))
+    for clip in clips:
+        print('clip = {}'.format(clip))
 
-            path_out_game = os.path.join(path_output, game)
-            if not os.path.exists(path_out_game):
-                os.makedirs(path_out_game)
+        path_out_game = os.path.join(path_output, game)
+        if not os.path.exists(path_out_game):
+            os.makedirs(path_out_game)
 
-            path_out_clip = os.path.join(path_out_game, clip)    
-            if not os.path.exists(path_out_clip):
-                os.makedirs(path_out_clip)  
+        path_out_clip = os.path.join(path_out_game, clip)    
+        if not os.path.exists(path_out_clip):
+            os.makedirs(path_out_clip)  
 
-            path_labels = os.path.join(os.path.join(path_input, game, clip), 'Label.csv')
-            labels = pd.read_csv(path_labels)    
-            for idx in range(labels.shape[0]):
-                file_name, vis, x, y, _ = labels.loc[idx, :]
-                heatmap = np.zeros((height, width, 3), dtype=np.uint8)
-                if vis != 0:
-                    x = int(x)
-                    y = int(y)
+        path_labels = os.path.join(os.path.join(path_input, game, clip), 'Label.csv')
+        labels = pd.read_csv(path_labels)    
+        for idx in range(labels.shape[0]):
+            file_name, vis, x, y, _ = labels.loc[idx, :]
+            heatmap = np.zeros((height, width, 3), dtype=np.uint8)
+            if vis in [1, 2]:  
+                x = int(float(x)) if not pd.isna(x) else None
+                y = int(float(y)) if not pd.isna(y) else None
+                if x is not None and y is not None:
                     for i in range(-size, size+1):
                         for j in range(-size, size+1):
-                                if x+i<width and x+i>=0 and y+j<height and y+j>=0 :
-                                    temp = gaussian_kernel_array[i+size][j+size]
-                                    if temp > 0:
-                                        heatmap[y+j,x+i] = (temp,temp,temp)
+                            if x+i<width and x+i>=0 and y+j<height and y+j>=0:
+                                temp = gaussian_kernel_array[i+size][j+size]
+                                if temp > 0:
+                                    heatmap[y+j,x+i] = (temp,temp,temp)
 
-                cv2.imwrite(os.path.join(path_out_clip, file_name), heatmap) 
-                
+            cv2.imwrite(os.path.join(path_out_clip, file_name), heatmap)
+
 def create_gt_labels(path_input, path_output, train_rate=0.7):
     df = pd.DataFrame()
-    for game_id in range(1,11):
-        game = 'game{}'.format(game_id)
-        clips = os.listdir(os.path.join(path_input, game))
-        for clip in clips:
-            labels = pd.read_csv(os.path.join(path_input, game, clip, 'Label.csv'))
-            labels['gt_path'] = 'gts/' + game + '/' + clip + '/' + labels['file name']
-            labels['path1'] = 'images/' + game + '/' + clip + '/' + labels['file name']
-            labels_target = labels[2:]
-            labels_target.loc[:, 'path2'] = list(labels['path1'][1:-1])
-            labels_target.loc[:, 'path3'] = list(labels['path1'][:-2])
-            df = df.append(labels_target)
-    df = df.reset_index(drop=True) 
-    df = df[['path1', 'path2', 'path3', 'gt_path', 'x-coordinate', 'y-coordinate', 'status', 'visibility']]
-    df = df.sample(frac=1)
+    game = 'game1'  # Assuming we're processing game1 directory
+    clips = os.listdir(os.path.join(path_input, game))
+    
+    for clip in clips:
+        labels = pd.read_csv(os.path.join(path_input, game, clip, 'Label.csv'))
+        
+        # Create a copy of the DataFrame to avoid SettingWithCopyWarning
+        labels = labels.copy()
+        
+        # Add new columns
+        labels['gt_path'] = 'gts/' + game + '/' + clip + '/' + labels['File Name']
+        labels['path1'] = 'images/' + game + '/' + clip + '/' + labels['File Name']
+        
+        # Create paths for previous frames
+        path1_series = labels['path1'].tolist()
+        path2_series = path1_series[1:] + [None]  # Add None for last row
+        path3_series = path1_series[2:] + [None, None]  # Add None for last two rows
+        
+        # Create new DataFrame with all required columns
+        labels_target = pd.DataFrame({
+            'path1': path1_series[2:],
+            'path2': path2_series[2:],
+            'path3': path3_series[2:],
+            'gt_path': labels['gt_path'][2:],
+            'X': labels['X'][2:],
+            'Y': labels['Y'][2:],
+            'Trajectory Pattern': labels['Trajectory Pattern'][2:],
+            'Visibility Class': labels['Visibility Class'][2:]
+        })
+        
+        df = pd.concat([df, labels_target], ignore_index=True)
+    
+    df = df.sample(frac=1)  # Shuffle the data
     num_train = int(df.shape[0]*train_rate)
     df_train = df[:num_train]
     df_test = df[num_train:]
+    
     df_train.to_csv(os.path.join(path_output, 'labels_train.csv'), index=False)
-    df_test.to_csv(os.path.join(path_output, 'labels_val.csv'), index=False)  
-
+    df_test.to_csv(os.path.join(path_output, 'labels_val.csv'), index=False)
 
 if __name__ == '__main__':
-    SIZE = 20
-    VARIANCE = 10
+    SIZE = 15
+    VARIANCE = 8
     WIDTH = 1280
     HEIGHT = 720   
 
